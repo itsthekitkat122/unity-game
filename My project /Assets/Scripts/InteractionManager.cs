f8dac1c594f1b4cfa38d3912fd898548173f6ca4
techtalk <100383021+itsthekitkat122@users.noreply.github.com>
2026-06-17 10:32:20 +1200

    iffy commit

---
+++ My project /Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+public static InteractionManager Instance {get; set; }
+private void Awake()
+    {
+        if(Instance != null && Instance!= this)
+        {
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            Instance = this;
+        } 
+    }
+    private void Update()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject objectHitByRaycast = hit.transform.gameObject;
+
+            if (objectHitByRaycast.GetComponent<Weapon>())
+            {
+                
+            }
+        }
+    }
+}
